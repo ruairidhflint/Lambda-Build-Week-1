c5a6a05755002aab5bb24181eeaafe96853e3202
Rory Flint <rory@roryflint.co.uk>
2019-05-20 17:18:23 +0100

    added JS to submit form

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 
 let inputs = document.querySelectorAll('input');
-
+let input1 = document.querySelector('#input-one');
+let input2 = document.querySelector('#input-two');
+let submitButton = document.querySelector('button');
+let form = document.querySelector('.right-bottom');
+let altForm = document.querySelector('.right-bottom-alt');
 inputs.forEach(input => input.required = true);
+
+submitButton.addEventListener('click', logDetails);
+submitButton.addEventListener('click', submitted)
+
+
+
+function logDetails() {
+    let email = input1.value;
+    let password = input2.value;
+
+    console.log(email + " " + password)   
+}
+
+
+function submitted() {
+    form.style = "display: none";
+    altForm.style = "display: block"
+
+}
+
+
 
