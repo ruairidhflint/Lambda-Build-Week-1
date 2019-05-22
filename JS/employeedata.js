c4132246177aa7a45179b33fde17c0e648353954
Rory Flint <rory@roryflint.co.uk>
2019-05-22 09:18:50 +0100

    about us page updated

--- JS/employeedata.js
+++ JS/employeedata.js
@@ -1,8 +1,14 @@
 let justinBio = "Born and raised in Paris, France, Justin studied computer science at University and embarked on a career of software development. Along with his fellow creators, his struggle with data management throughout his studies let him to Pintereach and the product you see today. When not craning his neck at code, he can be found walking his dogs and judging bad coffee."
 let tommyBio = "Specialising in all things design, art and fanciful, Tommy spends his days disecting colour schemes, margins and typography. Nothing grinds his gears more than misplaced symmetry or ill matched colours. Can usually be found hiding his instant coffee from Justin."
 let samirBio = "When your boards give you a perfect set of metadata, source code and keywording, Samir is the man to thank. Backend data management and optimisation keep him up at night and Big O is burnt into the back of his eye lids. Samir once held the world record for most Drake lyrics memorised. Losing out eventually to Drake himself."
-const employees = [{name: "Justin Wellers", bio: justinBio, photo: "IMG/headshot1.jpg"},
-{name: "Tommy Segura", bio: tommyBio, photo: "IMG/headshot3.jpg"}, {name: "Samir Syed", bio: samirBio, photo: "IMG/headshot2.jpeg"}];
+
+const employees1 = [{name: "Justin Wellers", bio: justinBio, photo: "IMG/headshot1.jpg", include: 'yes'},
+{name: "Tommy Segura", bio: tommyBio, photo: "IMG/headshot3.jpg", include: 'yes'}, {name: "Samir Syed", bio: samirBio, photo: "IMG/headshot2.jpeg", include: "yes"},
+ {name: 'Ruairidh Flint', bio: "null", photo: "IMG/pp.jpeg", include: "no"}];
+
+let employees = employees1.filter(x => {
+    return x.include == "yes";
+})
 
 
 const docLocation = document.querySelector('.about-container');
