eead8d4d61f8b34e404d5d71f3a1618a150fefa4
Rory Flint <rory@roryflint.co.uk>
2019-05-24 09:00:22 +0100

    side nav bar added

---
+++ JS/sidenav.js
@@ -0,0 +1,20 @@
+let hamburgerButton = document.querySelector('.hamburger');
+let sideNav = document.querySelector('.side-nav');
+let closeButton = document.querySelector('.close');
+
+hamburgerButton.addEventListener('click', openNav);
+closeButton.addEventListener('click', closeNav);
+window.addEventListener('resize', closeNav);
+
+
+function openNav(){
+    sideNav.style.width = "60vw";
+    sideNav.style.transition = "1s";
+}
+
+function closeNav(){
+    if (sideNav.style.width != "0px"){
+    sideNav.style.width = "0px";
+    sideNav.style.transition = "1s";
+    }
+}
